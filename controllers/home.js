dede42815240baf4a7770abdd9de5746764af623
mlautaro1 <lautaromoreno@gmail.com>
2022-12-07 23:52:30 -0300

    fixed home controller

--- controllers/home.js
+++ controllers/home.js
@@ -1,13 +1,5 @@
-const MySchema = require('../models/mymodels.js')
-
 module.exports = {
-    getIndex: async (req,res)=>{
-        try{
-            const Items = await MySchema.find()
-            const itemsLeft = await MySchema.countDocuments({completed: false})
-            res.render('index.ejs', {})
-        }catch(err){
-            console.log(err)
-        }
-    }
-}
+    getIndex: (req, res) => {
+      res.render("index.ejs");
+    },
+  };
